1bc6d9cd71b5c029b7141f594357a19ae3e9e770
ktortolini <134340991+ktortolini@users.noreply.github.com>
2025-01-15 14:05:47 -0600

    Add index dot js file in /js/ with boilerplate from class lecture

---
+++ js/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+(() => {
+  function handleClick() {
+    console.log("Thank you for clicking me!");
+  }
+
+  document.querySelector("#test-button").addEventListener("click", handleClick);
+})();
