1d953f379d9083559fbb7cdf1bbfd45f44ac7db6
ktortolini <134340991+ktortolini@users.noreply.github.com>
2025-01-16 14:31:50 -0600

    Remove various files with re-organization of project structure from class lecture

--- js/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-(() => {
-  function handleClick() {
-    console.log("Thank you for clicking me!");
-  }
-
-  document.querySelector("#test-button").addEventListener("click", handleClick);
-})();
