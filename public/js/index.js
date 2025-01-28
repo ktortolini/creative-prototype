c16b4896cd4860e9f5a45acce88dd8d6b1773e3c
ktortolini <134340991+ktortolini@users.noreply.github.com>
2025-01-28 16:43:55 -0600

    Update index dot js file in /js/ with boilerplate form class lecture

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,3 @@
-'use strict';
-
-(() => {
-	function handleClick() {
-		console.log('Thank you for clicking me!');
-	}
-
-	document.querySelector('#test-button').addEventListener(
-		'click',
-		handleClick,
-	);
+(function () {
+	'use strict';
 })();
