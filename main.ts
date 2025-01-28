c6c569664aae7c9bd65691481b361041546cb387
ktortolini <134340991+ktortolini@users.noreply.github.com>
2025-01-28 16:43:08 -0600

    Update main dot ts file with additional boilerplate from class lecture

--- main.ts
+++ main.ts
@@ -1,12 +1,25 @@
 import express from '@express';
+import * as utils from './utils/utils.ts';
 
 const app: express.Application = express();
 const port: number = 8000;
 
+app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/', (_: express.Request, res: express.Response) => {
 	res.sendFile('index.html');
+});
+
+app.post('/mail', async (req: express.Request, res: express.Response) => {
+	await utils
+		.sendMessage(req.body.sub, req.body.txt)
+		.then(() => {
+			res.send({ result: 'success' });
+		})
+		.catch((_: Error) => {
+			res.send({ result: _.message });
+		});
 });
 
 app.listen(port, () => {
