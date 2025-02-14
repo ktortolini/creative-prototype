00c5e528f1964a564657505bbc221de09beaec90
ktortolini <134340991+ktortolini@users.noreply.github.com>
2025-02-13 19:43:40 -0600

    Update index dot js and navbar dot ejs files with an id and wallet connect functionality for web3 transactions using metamask

--- public/js/index.js
+++ public/js/index.js
@@ -2,9 +2,27 @@
 	'use strict';
 
 	/**
+	 * Initial wallet connection
+	 */
+
+	connectWallet();
+
+	/**
+	 * Add the user address variable
+	 */
+
+	let userAddress = null;
+
+	/**
 	 * Add the animate function from the global motion instance
 	 */
 	const { animate } = Motion;
+
+	/**
+	 * Add the connect wallet button element using specifiers
+	 */
+
+	const connect = document.getElementById('wallet-connect');
 
 	/**
 	 * Add the hero content element using specifiers
@@ -31,5 +49,38 @@
 			duration: 0.75,
 			easing: 'ease-linear',
 		});
+	}
+
+	/**
+	 * Add an event listener to the connect wallet button element
+	 */
+
+	connect.addEventListener('click', () => {
+		connectWallet();
+	});
+
+	async function connectWallet() {
+		await globalThis.ethereum
+			.request({ method: 'eth_requestAccounts' })
+			.then((data) => {
+				userAddress = data[0];
+				const walletString = userAddress.substring(0, 5) + '...' +
+					userAddress.substring(38, 42);
+				connect.innerHTML = walletString;
+				return userAddress;
+			})
+			.catch((_) => {
+				if (_.code === 4001) {
+					/**
+					 * 4001 - The user rejected the connection request.
+					 */
+					// deno-fmt-ignore
+					console.info(
+						'🧯 ' + 'Please connect a wallet.',
+					);
+				} else {
+					console.error(_.message);
+				}
+			});
 	}
 })();
