422c779228e6309d094348501221e1a22214eea6
ktortolini <134340991+ktortolini@users.noreply.github.com>
2025-03-02 13:22:06 -0600

    Update deno dot json and main dot ts files with addition of cors package to handle cors policy

--- main.ts
+++ main.ts
@@ -1,14 +1,15 @@
+import cors from '@cors';
 import express from '@express';
 import * as utils from './utils/utils.ts';
 import * as db from './utils/database.ts';
 import { blue, green } from '@std/fmt/colors';
-
 // deno-lint-ignore no-explicit-any
 let projects: any[] = [];
 
 const app: express.Application = express();
 const port: number = 8000;
 
+app.use(cors());
 app.use(express.json());
 app.set('views', './views');
 app.set('view engine', 'ejs');
@@ -30,12 +31,17 @@ app.get('/', async (_: express.Request, res: express.Response, next: express.Nex
 		});
 });
 
-app.get('/project', (_: express.Request, res: express.Response) => {
-	res.render('project.ejs', { projectArray: projects });
-});
-
 app.get('/projects', (_: express.Request, res: express.Response) => {
 	res.render('projects.ejs', { projectArray: projects });
+});
+
+app.get('/project/:id', (req: express.Request, res: express.Response) => {
+	const id = req.params.id;
+	if (id > projects.length) {
+		throw new Error('No project with that ID');
+	}
+	console.log(projects[id - 1], id);
+	res.render('project.ejs', { project: projects[id - 1], which: id });
 });
 
 app.get('/contact', (_: express.Request, res: express.Response) => {
@@ -52,6 +58,21 @@ app.post('/mail', async (req: express.Request, res: express.Response) => {
 			res.send({ result: '🧯 ' + _.message });
 		});
 });
+
+app.use(
+	// deno-fmt-ignore
+	// deno-lint-ignore no-unused-vars
+	async (err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+		console.log(err);
+		let msg: string;
+		msg = err.message;
+		if (msg != 'No project with that ID') {
+			msg =
+				'There was an internal error. Apologies. We are working on cleaning up the mess.';
+		}
+		await res.render('error.ejs', { msg: msg });
+	},
+);
 
 app.listen(port, () => {
 	// deno-fmt-ignore
